294ff2913a3da35513f60ee9acbae6901284de47
Francisco Suero <suerofrancisco@yahoo.com>
2016-12-07 13:15:34 -0500

    agh

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create 
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      sign_in(@user)
+      flash[:success] = "You've successfully signed in"
+      redirect_to root_url 
+    else
+      flash.now[:error] = "We couldn't sign you in"
+      render :new
+    end
+  end
+
+  def destroy 
+    sign_out 
+    flash[:success] = "You signed out! Come back soon poke-user!"
+    redirect_to root_url
+  end
+
+end
